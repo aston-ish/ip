6733be0a19bb5cbc4a678550fc914afd613cb5a1
Aston <e1525454@u.nus.edu>
2026-08-21 13:04:40 +0800

    Add Task.java and update Topaz.java to use task objects

---
+++ src/main/java/Task.java
@@ -0,0 +1,45 @@
+/**
+ * Represents a task entered by the user.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description the task description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /** Marks this task as done. */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /** Marks this task as not done. */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the status icon used when displaying this task.
+     *
+     * @return {@code X} for a completed task, otherwise a blank space
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Returns this task's description.
+     *
+     * @return the task description
+     */
+    public String getDescription() {
+        return description;
+    }
+}
