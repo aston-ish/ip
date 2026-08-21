481a636471624e0fe47b42dee20fe2d9f40fd28b
Aston <e1525454@u.nus.edu>
2026-08-21 12:24:42 +0800

    Rename chatbot from Duke to Topaz

---
+++ src/main/java/Topaz.java
@@ -0,0 +1,11 @@
+public class Topaz {
+    public static void main(String[] args) {
+        String banner = " _____                 _          \n"
+                + "|_   _|__  _ __   __ _| |__       \n"
+                + "  | |/ _ \\| '_ \\ / _` | '_ \\      \n"
+                + "  | | (_) | |_) | (_| | | | |     \n"
+                + "  |_|\\___/| .__/ \\__,_|_| |_|     \n"
+                + "           |_|                      \n";
+        System.out.println(banner);
+    }
+}
