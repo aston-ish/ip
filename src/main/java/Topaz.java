5511d5eaa25517cc2a8353a79cfbfbcec192789b
Aston <e1525454@u.nus.edu>
2026-08-21 12:27:17 +0800

    Update Topaz.java to greet user and exit

--- src/main/java/Topaz.java
+++ src/main/java/Topaz.java
@@ -1,11 +1,19 @@
 public class Topaz {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = " _____                 _          \n"
                 + "|_   _|__  _ __   __ _| |__       \n"
                 + "  | |/ _ \\| '_ \\ / _` | '_ \\      \n"
                 + "  | | (_) | |_) | (_| | | | |     \n"
                 + "  |_|\\___/| .__/ \\__,_|_| |_|     \n"
                 + "           |_|                      \n";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Topaz.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
